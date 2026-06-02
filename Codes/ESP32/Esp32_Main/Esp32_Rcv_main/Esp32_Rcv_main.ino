6cfdbb965c26636f86736676a6e0df65bc94da69
CharlotteXIII <sippanon2546@gmail.com>
2026-06-02 16:26:58 +0700

    maincode before big change1

--- Codes/ESP32/Esp32_Main/Esp32_Rcv_main/Esp32_Rcv_main.ino
+++ Codes/ESP32/Esp32_Main/Esp32_Rcv_main/Esp32_Rcv_main.ino
@@ -2,7 +2,7 @@
 //  Core 0 : Receive ESP-NOW put into Queue
 //  Core 1 : Receive Queue and command servo through PCA9685 (X2)
 //
-//  PCA9685#1 (0x40) - MG90s  ch 0-15
+//  PCA9685#1 (0x40) - MG90s  ch 0-11
 //  PCA9685#2 (0x41) - MG996R ch 0-9
 //
 //  Commands:
@@ -23,8 +23,8 @@
 #include <Adafruit_Sensor.h>
 #include <Adafruit_BNO055.h>
 
-Adafruit_PWMServoDriver pca1 = Adafruit_PWMServoDriver(0x40);
-Adafruit_PWMServoDriver pca2 = Adafruit_PWMServoDriver(0x41);
+Adafruit_PWMServoDriver pca1 = Adafruit_PWMServoDriver(0x40); //MG90S
+Adafruit_PWMServoDriver pca2 = Adafruit_PWMServoDriver(0x41); //MG996R
 Adafruit_BNO055 bno = Adafruit_BNO055(55, 0x28);
 
 #define SERVO_MIN  150
@@ -32,6 +32,11 @@ Adafruit_BNO055 bno = Adafruit_BNO055(55, 0x28);
 #define PWM_FREQ   50
 
 QueueHandle_t commandQueue; //Create Queue
+volatile int stateLock = 0;
+volatile int zone = -1;
+
+float cur_pca1[12] = {0.0f};
+float cur_pca2[10] = {0.0f};
 
 //Create a box that stored int command from Controller
 typedef struct struct_message {
@@ -45,6 +50,7 @@ void pca1_SetAngle(uint8_t ch, float angle) {
   if (angle > 180.0f) angle = 180.0f;
   uint16_t pulse = SERVO_MIN + (uint16_t)((angle / 180.0f) * (SERVO_MAX - SERVO_MIN));
   pca1.setPWM(ch, 0, pulse);
+  cur_pca1[ch] = angle;
 }
 
 void pca2_SetAngle(uint8_t ch, float angle) {
@@ -52,90 +58,207 @@ void pca2_SetAngle(uint8_t ch, float angle) {
   if (angle > 180.0f) angle = 180.0f;
   uint16_t pulse = SERVO_MIN + (uint16_t)((angle / 180.0f) * (SERVO_MAX - SERVO_MIN));
   pca2.setPWM(ch, 0, pulse);
+  cur_pca2[ch] = angle;
+}
+//=============================================
+
+//===========Smooth Sweep======================
+void smoothSweepPair(Adafruit_PWMServoDriver &pca, uint8_t ch1, uint8_t ch2, float startAngle, float targetAngle, int stepDelay_ms) {
+  if (startAngle < targetAngle) {
+    for (float a = startAngle; a <= targetAngle; a += 1.0f) {
+      if (ch1 != 255) { if(&pca == &pca1) pca1_SetAngle(ch1, a); else pca2_SetAngle(ch1, a); }
+      if (ch2 != 255) { if(&pca == &pca1) pca1_SetAngle(ch2, a); else pca2_SetAngle(ch2, a); }
+      vTaskDelay(pdMS_TO_TICKS(stepDelay_ms));
+    }
+  } else {
+    for (float a = startAngle; a >= targetAngle; a -= 1.0f) {
+      if (ch1 != 255) { if(&pca == &pca1) pca1_SetAngle(ch1, a); else pca2_SetAngle(ch1, a); }
+      if (ch2 != 255) { if(&pca == &pca1) pca1_SetAngle(ch2, a); else pca2_SetAngle(ch2, a); }
+      vTaskDelay(pdMS_TO_TICKS(stepDelay_ms));
+    }
+  }
 }
 //=============================================
 
 //===============ACTIONs=======================
+
+void action_default() {
+  //Legs
+  smoothSweepPair(pca2, 4, 255, cur_pca2[4], 90.0f, 10);
+  smoothSweepPair(pca2, 5, 255, cur_pca2[5], 90.0f, 10);
+  smoothSweepPair(pca2, 6, 255, cur_pca2[6], 90.0f, 10);
+  smoothSweepPair(pca2, 7, 255, cur_pca2[7], 90.0f, 10);
+  smoothSweepPair(pca2, 8, 255, cur_pca2[8], 90.0f, 10);
+  smoothSweepPair(pca2, 9, 255, cur_pca2[9], 90.0f, 10);
+
+  //Arms
+  smoothSweepPair(pca2, 0, 255, cur_pca2[0], 150.0f, 10);
+  smoothSweepPair(pca2, 2, 255, cur_pca2[2], 60.0f, 10);
+  smoothSweepPair(pca2, 1, 255, cur_pca2[1]0.0f, 120.0f, 10);
+  smoothSweepPair(pca2, 3, 255, cur_pca2[3], 30.0f, 15);
+
+  //Shells
+  for (float a = 0.0f; a <= 90.0f; a += 2.0f) {
+    for(int i=0; i<12; i++) {
+      if (a >= cur_pca1[i]) pca1_SetAngle(i, a);
+    }
+    vTaskDelay(pdMS_TO_TICKS(10));
+  }
+}
+
 void action_up() {
-  Serial.printf("[CMD 1] UP — state %d\n", state_cmd1);
-  if (state_cmd1 == 0) {
-    pca1_SetAngle(0,   0.0f);
-    pca2_SetAngle(0,   0.0f);
-    state_cmd1 = 1;
+  if (stateLock == 1 && zone == 0)
+  {
+    // [Shells] //////////////////////////////
+    pca1_SetAngle( 0, 90.0f);
+    pca1_SetAngle( 1, 90.0f);
   }
-  else if (state_cmd1 == 1) {
-    pca1_SetAngle(0,  90.0f);
-    pca2_SetAngle(0,  90.0f);
-    state_cmd1 = 2;
+  else if (stateLock == 1 && zone == 1)
+  {
+    // [Shells] //////////////////////////////
+    pca1_SetAngle( 0, 90.0f);
+    pca1_SetAngle( 1, 90.0f);
   }
-  else if (state_cmd1 == 2) {
-    pca1_SetAngle(0, 180.0f);
-    pca2_SetAngle(0, 180.0f);
-    state_cmd1 = 0;
+  else if (stateLock == 1 && zone == 2)
+  {
+    // [Shells] //////////////////////////////
+    pca1_SetAngle( 0, 90.0f);
+    pca1_SetAngle( 1, 90.0f);
+  }
+  else if (stateLock == 1 && zone == 3)
+  {
+    // [Shells] //////////////////////////////
+    pca1_SetAngle( 0, 90.0f);
+    pca1_SetAngle( 1, 90.0f);
   }
 }
 
 void action_left() {
-  Serial.printf("[CMD 2] LEFT — state %d\n", state_cmd2);
-  if (state_cmd2 == 0) {
-    pca1_SetAngle(0,  0.0f);
-    pca2_SetAngle(0,  0.0f);
-    state_cmd2 = 1;
+  if (stateLock == 1 && zone == 0)
+  {
+    // [Shells] //////////////////////////////
+    pca1_SetAngle( 0, 90.0f);
+    pca1_SetAngle( 1, 90.0f);
   }
-  else if (state_cmd2 == 1) {
-    pca1_SetAngle(0, 90.0f);
-    pca2_SetAngle(0, 90.0f);
-    state_cmd2 = 0;
+  else if (stateLock == 1 && zone == 1)
+  {
+    // [Shells] //////////////////////////////
+    pca1_SetAngle( 0, 90.0f);
+    pca1_SetAngle( 1, 90.0f);
+  }
+  else if (stateLock == 1 && zone == 2)
+  {
+    // [Shells] //////////////////////////////
+    pca1_SetAngle( 0, 90.0f);
+    pca1_SetAngle( 1, 90.0f);
+  }
+  else if (stateLock == 1 && zone == 3)
+  {
+    // [Shells] //////////////////////////////
+    pca1_SetAngle( 0, 90.0f);
+    pca1_SetAngle( 1, 90.0f);
   }
 }
 
 void action_right() {
-  Serial.printf("[CMD 3] RIGHT — state %d\n", state_cmd3);
-  if (state_cmd3 == 0) {
-    pca1_SetAngle(0,  0.0f);
-    pca2_SetAngle(0,  0.0f);
-    state_cmd3 = 1;
+  if (stateLock == 1 && zone == 0)
+  {
+    // [Shells] //////////////////////////////
+    pca1_SetAngle( 0, 90.0f);
+    pca1_SetAngle( 1, 90.0f);
   }
-  else if (state_cmd3 == 1) {
-    pca1_SetAngle(0, 90.0f);
-    pca2_SetAngle(0, 90.0f);
-    state_cmd3 = 0;
+  else if (stateLock == 1 && zone == 1)
+  {
+    // [Shells] //////////////////////////////
+    pca1_SetAngle( 0, 90.0f);
+    pca1_SetAngle( 1, 90.0f);
+  }
+  else if (stateLock == 1 && zone == 2)
+  {
+    // [Shells] //////////////////////////////
+    pca1_SetAngle( 0, 90.0f);
+    pca1_SetAngle( 1, 90.0f);
+  }
+  else if (stateLock == 1 && zone == 3)
+  {
+    // [Shells] //////////////////////////////
+    pca1_SetAngle( 0, 90.0f);
+    pca1_SetAngle( 1, 90.0f);
   }
 }
 
 void action_X() {
-  Serial.printf("[CMD 6] X — state %d\n", state_cmd6);
-  if (state_cmd6 == 0) {
-    pca1_SetAngle(0,   0.0f);
-    state_cmd6 = 1;
-  }
-  else if (state_cmd6 == 1) {
-    pca1_SetAngle(0,  90.0f);
-    state_cmd6 = 2;
-  }
-  else if (state_cmd6 == 2) {
-    pca1_SetAngle(0, 180.0f);
-    state_cmd6 = 0;
+  // Not Create stand up sequence yet
+  if (stateLock == 1){
+  // [LEGS]
+    smoothSweepPair(pca2, 6, 9, 0.0f, 90.0f, 10);
+    smoothSweepPair(pca2, 5, 8, 0.0f, 90.0f, 10);
+    smoothSweepPair(pca2, 4, 7, 0.0f, 90.0f, 10);
+  // [ARMS]
+    smoothSweepPair(pca2, 0, 2, 0.0f, 30.0f, 10);
+    smoothSweepPair(pca2, 1, 3, 0.0f, 30.0f, 10);
+
+  // [Shells] //////////////////////////////
+    for (float a = 0.0f; a <= 90.0f; a += 2.0f) {
+      for(int i=0; i<12; i++) { pca1_SetAngle(i, a); }
+      vTaskDelay(pdMS_TO_TICKS(10));
+    }
+    stateLock = 0;
   }
 }
 
 void action_O() {
-  Serial.printf("[CMD 7] O — state %d\n", state_cmd7);
-  if (state_cmd7 == 0) {
-    pca2_SetAngle(0,   0.0f);
-    state_cmd7 = 1;
-  }
-  else if (state_cmd7 == 1) {
-    pca2_SetAngle(0,  90.0f);
-    state_cmd7 = 2;
-  }
-  else if (state_cmd7 == 2) {
-    pca2_SetAngle(0, 180.0f);
-    state_cmd7 = 0;
+  if (stateLock == 0)
+  {  
+    stateLock = 1; 
+    // [LEGS] //////////////////////////////
+    smoothSweepPair(pca2, 4, 255, 90.0f, 0.0f, 15);
+    smoothSweepPair(pca2, 7, 255, 90.0f, 180.0f, 15);
+    vTaskDelay(pdMS_TO_TICKS(300));
+
+    smoothSweepPair(pca2, 5, 255, 90.0f, 30.0f, 15);
+    smoothSweepPair(pca2, 4, 255, 90.0f, 120.0f, 15);
+    vTaskDelay(pdMS_TO_TICKS(500));
+
+    smoothSweepPair(pca2, 6, 255, 90.0f, 60.0f, 15);
+    smoothSweepPair(pca2, 9, 255, 90.0f, 150.0f, 15);
+    vTaskDelay(pdMS_TO_TICKS(300));
+
+    // [ARMS] //////////////////////////////
+    smoothSweepPair(pca2, 0, 255, 90.0f, 180.0f, 15);
+    smoothSweepPair(pca2, 2, 255, 90.0f, 0.0f, 15);
+    vTaskDelay(pdMS_TO_TICKS(500));
+    
+    smoothSweepPair(pca2, 1, 255, 90.0f, 150.0f, 15);
+    smoothSweepPair(pca2, 3, 255, 90.0f, 60.0f, 15);
+    vTaskDelay(pdMS_TO_TICKS(500));
+
+    // [Shells] //////////////////////////////
+    for (float a = 90.0f; a >= 0.0f; a -= 2.0f) {
+      for(int i=0; i<12; i++) { pca1_SetAngle(i, a); }
+      vTaskDelay(pdMS_TO_TICKS(10));
+    }
   }
 }
 //=============================================
 
+//===============BNO055=======================
+void bnoTask(void *pvParameters) {
+  vTaskDelay(pdMS_TO_TICKS(500));
+  for (;;) {
+    sensors_event_t orientEvent;
+    bno.getEvent(&orientEvent, Adafruit_BNO055::VECTOR_EULER);
+    float roll = orientEvent.orientation.z;
+
+    if      (roll > -30  && roll <= 30)   zone = 0;
+    else if (roll > 30   && roll <= 135)  zone = 1;
+    else if (roll > 135  || roll < -135)  zone = 2;
+    else if (roll >= -135 && roll <= -30) zone = 3;
+
+    // Serial.printf("[BNO] Roll: %.1f → Zone: %d\n", roll, zone);
+    vTaskDelay(pdMS_TO_TICKS(100));
+  }
+}
 
 // ================================================================
 // CORE 0 — ESP-NOW callback
@@ -153,10 +276,11 @@ void OnDataRecv(const uint8_t *mac, const uint8_t *incomingData, int len) {
 void servoTask(void *pvParameters) {
   int cmd;
   vTaskDelay(pdMS_TO_TICKS(200));
-  action_idle();
+  action_default();
 
   for (;;) {
     if (xQueueReceive(commandQueue, &cmd, portMAX_DELAY) == pdTRUE) {
+      
       switch (cmd) {
         case 1: action_up();    break;
         case 2: action_left();  break;
@@ -177,14 +301,21 @@ void setup() {
   Wire.begin(21, 22);
   pca1.begin(); pca1.setPWMFreq(PWM_FREQ);
   pca2.begin(); pca2.setPWMFreq(PWM_FREQ);
+  
+  if (!bno.begin()) {
+    Serial.println("BNO055 not found");
+    while (1);
+  }
+  bno.setExtCrystalUse(true);
 
   commandQueue = xQueueCreate(5, sizeof(int));
   if (commandQueue == NULL) {
-    Serial.println("Queue create failed!");
+    Serial.println("Queue create failed");
     while (1);
   }
 
   xTaskCreatePinnedToCore(servoTask, "ServoTask", 4096, NULL, 1, NULL, 1);
+  xTaskCreatePinnedToCore(bnoTask,   "BNOTask",   4096, NULL, 1, NULL, 0);
 
   WiFi.mode(WIFI_STA);
   if (esp_now_init() != ESP_OK) {
@@ -196,5 +327,5 @@ void setup() {
 }
 
 void loop() {
-  vTaskDelay(pdMS_TO_TICKS(1000));
+  vTaskDelay(pdMS_TO_TICKS(500));
 }
